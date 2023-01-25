8f11253d50445404b829a83cd0589c314d63c8c8
DK <eastflag@cnspartner.com>
2023-01-25 17:19:27 +0900

    4강. PageView 작동안함.

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'intro_page.dart';
+
 class AuthScreen extends StatelessWidget {
-  const AuthScreen({Key? key}) : super(key: key);
+  AuthScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Colors.green,
-      body: Center(
-        child: Text('로그인을 하십시요.'),
-      ),
+      // body: PageView(
+      //   children: [
+      //     IntroPage(),
+      //     Container(color: Colors.accents[2],),
+      //     Container(color: Colors.accents[3],),
+      //   ]
+      // ),
+      body: IntroPage(),
     );
   }
+
+  final _controller = PageController(
+    initialPage: 0,
+  );
 }
