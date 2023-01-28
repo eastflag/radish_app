c2fcc9b6b763e4324f6dc0062d49dab77ecb7530
eastflag <eastflag@gmail.com>
2023-01-28 13:03:34 +0900

    9강 완 - ListView, ListTile

--- lib/screens/address_page.dart
+++ lib/screens/address_page.dart
@@ -1,3 +1,4 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,18 +19,27 @@ class AddressPage extends StatelessWidget {
                 border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
                 hintStyle: TextStyle(color: Theme.of(context).hintColor)),
           ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                TextButton.icon(
-                    onPressed: () => {},
-                    icon: Icon(CupertinoIcons.compass, color: Colors.white),
-                    label: Text("현재 위치로 찾기", style: Theme.of(context).textTheme.button),
-                    style: TextButton.styleFrom(backgroundColor: Theme.of(context).primaryColor))
-              ],
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextButton.icon(
+                  onPressed: () => {},
+                  icon: Icon(CupertinoIcons.compass, color: Colors.white),
+                  label: Text("현재 위치로 찾기", style: Theme.of(context).textTheme.button),
+                  style: TextButton.styleFrom(backgroundColor: Theme.of(context).primaryColor))
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 30,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                      leading: ExtendedImage.asset('assets/images/carrot.jpg'),
+                      title: Text('title $index'),
+                      subtitle: Text('subtitle: $index'),
+                    trailing: Icon(Icons.more),
+                  );
+                }),
           )
         ],
       ),
