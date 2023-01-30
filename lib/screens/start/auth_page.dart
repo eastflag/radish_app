44efed4ac9f8f329ab669bdf40370107472863f1
eastflag <eastflag@gmail.com>
2023-01-30 20:39:13 +0900

    10강 - TextFormField 숫자타입, formatter

--- lib/screens/start/auth_page.dart
+++ lib/screens/start/auth_page.dart
@@ -1,11 +1,13 @@
 import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
 import 'package:radish_app/constants/common_size.dart';
 
 class AuthPage extends StatelessWidget {
   AuthPage({Key? key}) : super(key: key);
 
   final inputBorder = OutlineInputBorder(borderSide: BorderSide(color: Colors.grey));
+  TextEditingController _textEditingController = TextEditingController(text: "010");
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +33,21 @@ class AuthPage extends StatelessWidget {
                     height: size.width * 0.25,
                   ),
                   SizedBox(width: 10.0,),
-                  Text('무마켓은 전화번호로 가입합니다. \n여러분의 개인정보는 안전히 보관되며\n외부에 노출되지 않습니다.')
+                  Expanded(
+                    child: Text(
+                      '무마켓은 전화번호로 가입합니다. 여러분의 개인정보는 안전히 보관되며 외부에 노출되지 않습니다.',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                  )
                 ],
               ),
-              SizedBox(height: common_sm_padding,),
+              SizedBox(
+                height: common_sm_padding,
+              ),
               TextFormField(
                 keyboardType: TextInputType.phone,
+                controller: _textEditingController,
+                inputFormatters: [MaskedInputFormatter("000-0000-0000")],
                 decoration: InputDecoration(
                   border: inputBorder,
                   focusedBorder: inputBorder,
