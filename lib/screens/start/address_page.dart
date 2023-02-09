dcc642e0e8a4bbe56e7b1c0bd5b5e3d90bcca938
DK <eastflag@cnspartner.com>
2023-02-09 16:02:52 +0900

    16강 - location 을 geolocator 플러그인으로 변경

--- lib/screens/start/address_page.dart
+++ lib/screens/start/address_page.dart
@@ -2,7 +2,7 @@ import 'package:extended_image/extended_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:radish_app/screens/start/address_service.dart';
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 
 import '../../model/AddressModel.dart';
 import 'intro_page.dart';
@@ -43,30 +43,43 @@ class _AddressPageState extends State<AddressPage> {
           ),
           TextButton.icon(
               onPressed: () async {
-                Location location = Location();
+                bool serviceEnabled;
+                LocationPermission permission;
 
-                bool _serviceEnabled;
-                PermissionStatus _permissionGranted;
-                LocationData _locationData;
+                // Test if location services are enabled.
+                serviceEnabled = await Geolocator.isLocationServiceEnabled();
+                if (!serviceEnabled) {
+                  // Location services are not enabled don't continue
+                  // accessing the position and request users of the
+                  // App to enable the location services.
+                  return Future.error('Location services are disabled.');
+                }
 
-                _serviceEnabled = await location.serviceEnabled();
-                if (!_serviceEnabled) {
-                  _serviceEnabled = await location.requestService();
-                  if (!_serviceEnabled) {
-                    return;
+                permission = await Geolocator.checkPermission();
+                if (permission == LocationPermission.denied) {
+                  permission = await Geolocator.requestPermission();
+                  if (permission == LocationPermission.denied) {
+                    // Permissions are denied, next time you could try
+                    // requesting permissions again (this is also where
+                    // Android's shouldShowRequestPermissionRationale
+                    // returned true. According to Android guidelines
+                    // your App should show an explanatory UI now.
+                    return Future.error('Location permissions are denied');
                   }
                 }
 
-                _permissionGranted = await location.hasPermission();
-                if (_permissionGranted == PermissionStatus.denied) {
-                  _permissionGranted = await location.requestPermission();
-                  if (_permissionGranted != PermissionStatus.granted) {
-                    return;
-                  }
+                if (permission == LocationPermission.deniedForever) {
+                  // Permissions are denied forever, handle appropriately.
+                  return Future.error(
+                      'Location permissions are permanently denied, we cannot request permissions.');
                 }
 
-                _locationData = await location.getLocation();
-                logger.d(_locationData);
+                // When we reach here, permissions are granted and we can
+                // continue accessing the position of the device.
+                Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+                logger.d('${position.altitude}, ${position.longitude}');
+
+                AddressService().findAddressByCoordinate(log: position.altitude, lat: position.longitude);
               },
               icon: Icon(CupertinoIcons.compass, color: Colors.white),
               label: Text("현재 위치로 찾기", style: Theme.of(context).textTheme.button),
--- lib/screens/start/address_service.dart
+++ lib/screens/start/address_service.dart
@@ -5,7 +5,7 @@ import 'package:radish_app/model/AddressModel.dart';
 import 'intro_page.dart';
 
 class AddressService {
-  Future<AddressModel> SearchAddressByStr (String text) async {
+  Future<AddressModel> SearchAddressByStr(String text) async {
     final formData = {
       // 'service': 'search',
       'key': VWORLD_KEY,
@@ -25,5 +25,24 @@ class AddressService {
     logger.d(addressModel);
 
     return addressModel;
+  }
+
+  Future<void> findAddressByCoordinate({required double log, required double lat}) async {
+    final Map<String, dynamic> formData = {
+      // 'service': 'search',
+      'key': VWORLD_KEY,
+      'service': 'address',
+      'request': 'GetAddress',
+      'type': 'BOTH',
+      'point': '$log, $lat',
+    };
+
+    final response = await Dio().get('http://api.vworld.kr/req/address', queryParameters: formData).catchError((e) {
+      logger.e(e.message);
+    });
+
+    logger.d(response);
+
+    return;
   }
 }
