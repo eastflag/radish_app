a7ae97aba9d6a0bab1ab74694d33ef25ab4cfd40
DK <eastflag@cnspartner.com>
2023-02-10 18:09:39 +0900

    19강 - 키보드 바깥쪽 클릭시 키보드 내리기

--- lib/screens/start/address_page.dart
+++ lib/screens/start/address_page.dart
@@ -35,122 +35,127 @@ class _AddressPageState extends State<AddressPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       minimum: EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          TextFormField(
-            onFieldSubmitted: (text) async {
-              _addressPointModelList.clear();
-
-              _addressModel = await AddressService().SearchAddressByStr(text);
-              setState(() {
-
-              });
-            },
-            controller: _addressController,
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search, color: Colors.grey),
-                prefixIconConstraints: BoxConstraints(minWidth: 24, minHeight: 24),
-                hintText: '도로명으로 검색하세요',
-                border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
-                hintStyle: TextStyle(color: Theme.of(context).hintColor)),
-          ),
-          TextButton.icon(
-              onPressed: () async {
-                _addressModel = null;
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              onFieldSubmitted: (text) async {
                 _addressPointModelList.clear();
+
+                _addressModel = await AddressService().SearchAddressByStr(text);
                 setState(() {
-                  _isGettingLocation = true;
-                });
 
-                bool serviceEnabled;
-                LocationPermission permission;
-
-                // Test if location services are enabled.
-                serviceEnabled = await Geolocator.isLocationServiceEnabled();
-                if (!serviceEnabled) {
-                  // Location services are not enabled don't continue
-                  // accessing the position and request users of the
-                  // App to enable the location services.
-                  return Future.error('Location services are disabled.');
-                }
-
-                permission = await Geolocator.checkPermission();
-                if (permission == LocationPermission.denied) {
-                  permission = await Geolocator.requestPermission();
-                  if (permission == LocationPermission.denied) {
-                    // Permissions are denied, next time you could try
-                    // requesting permissions again (this is also where
-                    // Android's shouldShowRequestPermissionRationale
-                    // returned true. According to Android guidelines
-                    // your App should show an explanatory UI now.
-                    return Future.error('Location permissions are denied');
-                  }
-                }
-
-                if (permission == LocationPermission.deniedForever) {
-                  // Permissions are denied forever, handle appropriately.
-                  return Future.error(
-                      'Location permissions are permanently denied, we cannot request permissions.');
-                }
-
-                // When we reach here, permissions are granted and we can
-                // continue accessing the position of the device.
-                Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-                logger.d('${position.altitude}, ${position.longitude}');
-
-                List<AddressPointModel> addressList = await AddressService().findAddressByCoordinate(log: position.longitude, lat: position.latitude);
-                // List<AddressPointModel> addressList = await AddressService().findAddressByCoordinate(log: 126.977041, lat: 37.579617);
-
-                _addressPointModelList.addAll(addressList);
-
-                setState(() {
-                  _isGettingLocation = false;
                 });
               },
-              icon: _isGettingLocation ? CircularProgressIndicator(color: Colors.white) : Icon(CupertinoIcons.compass, color: Colors.white),
-              label: Text(_isGettingLocation ? "위치 찾는중 ..." : "현재 위치로 찾기", style: Theme.of(context).textTheme.button),
-              style: TextButton.styleFrom(backgroundColor: Theme.of(context).primaryColor)),
-          if (_addressModel != null) Expanded(
-            child: ListView.builder(
-                itemCount: (_addressModel?.result?.items == null ? 0 : _addressModel?.result?.items?.length),
-                itemBuilder: (context, index) {
-                  if (_addressModel == null || _addressModel?.result == null || _addressModel!.result!.items == null
-                  || _addressModel!.result!.items![index].address == null) {
-                    return Container();
+              controller: _addressController,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search, color: Colors.grey),
+                  prefixIconConstraints: BoxConstraints(minWidth: 24, minHeight: 24),
+                  hintText: '도로명으로 검색하세요',
+                  border: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey)),
+                  hintStyle: TextStyle(color: Theme.of(context).hintColor)),
+            ),
+            TextButton.icon(
+                onPressed: () async {
+                  _addressModel = null;
+                  _addressPointModelList.clear();
+                  setState(() {
+                    _isGettingLocation = true;
+                  });
+
+                  bool serviceEnabled;
+                  LocationPermission permission;
+
+                  // Test if location services are enabled.
+                  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+                  if (!serviceEnabled) {
+                    // Location services are not enabled don't continue
+                    // accessing the position and request users of the
+                    // App to enable the location services.
+                    return Future.error('Location services are disabled.');
                   }
 
-                  return ListTile(
-                    leading: ExtendedImage.asset('assets/images/carrot.jpg'),
-                    title: Text(_addressModel?.result?.items?[index].address?.road ?? ""),
-                    subtitle: Text(_addressModel?.result?.items?[index].address?.parcel ?? ""),
-                    trailing: Icon(Icons.more),
-                    onTap: () {
-                      _saveAddressOnSharedPreference(_addressModel?.result?.items?[index].address?.road ?? "");
-                      context.read<PageController>().animateToPage(2, duration: Duration(microseconds: 700), curve: Curves.easeOut);
+                  permission = await Geolocator.checkPermission();
+                  if (permission == LocationPermission.denied) {
+                    permission = await Geolocator.requestPermission();
+                    if (permission == LocationPermission.denied) {
+                      // Permissions are denied, next time you could try
+                      // requesting permissions again (this is also where
+                      // Android's shouldShowRequestPermissionRationale
+                      // returned true. According to Android guidelines
+                      // your App should show an explanatory UI now.
+                      return Future.error('Location permissions are denied');
                     }
-                  );
-                }),
-          ),
-          if (_addressPointModelList.isNotEmpty) Expanded(
-            child: ListView.builder(
-                itemCount: _addressPointModelList.length,
-                itemBuilder: (context, index) {
-                  if (_addressPointModelList[index].result == null || _addressPointModelList[index].result!.isEmpty) {
-                    return Container();
                   }
 
-                  return ListTile(
-                    title: Text(_addressPointModelList[index].result?[0].text ?? ""),
-                    subtitle: Text(_addressPointModelList[index].result?[0].zipcode ?? ""),
-                    onTap: () {
-                      _saveAddressOnSharedPreference(_addressPointModelList[index].result?[0].text ?? "");
-                      context.read<PageController>().animateToPage(2, duration: Duration(microseconds: 700), curve: Curves.easeOut);
+                  if (permission == LocationPermission.deniedForever) {
+                    // Permissions are denied forever, handle appropriately.
+                    return Future.error(
+                        'Location permissions are permanently denied, we cannot request permissions.');
+                  }
+
+                  // When we reach here, permissions are granted and we can
+                  // continue accessing the position of the device.
+                  Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+                  logger.d('${position.altitude}, ${position.longitude}');
+
+                  List<AddressPointModel> addressList = await AddressService().findAddressByCoordinate(log: position.longitude, lat: position.latitude);
+                  // List<AddressPointModel> addressList = await AddressService().findAddressByCoordinate(log: 126.977041, lat: 37.579617);
+
+                  _addressPointModelList.addAll(addressList);
+
+                  setState(() {
+                    _isGettingLocation = false;
+                  });
+                },
+                icon: _isGettingLocation ? CircularProgressIndicator(color: Colors.white) : Icon(CupertinoIcons.compass, color: Colors.white),
+                label: Text(_isGettingLocation ? "위치 찾는중 ..." : "현재 위치로 찾기", style: Theme.of(context).textTheme.button),
+                style: TextButton.styleFrom(backgroundColor: Theme.of(context).primaryColor)),
+            if (_addressModel != null) Expanded(
+              child: ListView.builder(
+                  itemCount: (_addressModel?.result?.items == null ? 0 : _addressModel?.result?.items?.length),
+                  itemBuilder: (context, index) {
+                    if (_addressModel == null || _addressModel?.result == null || _addressModel!.result!.items == null
+                    || _addressModel!.result!.items![index].address == null) {
+                      return Container();
                     }
-                  );
-                }),
-          )
-        ],
+
+                    return ListTile(
+                      leading: ExtendedImage.asset('assets/images/carrot.jpg'),
+                      title: Text(_addressModel?.result?.items?[index].address?.road ?? ""),
+                      subtitle: Text(_addressModel?.result?.items?[index].address?.parcel ?? ""),
+                      trailing: Icon(Icons.more),
+                      onTap: () {
+                        _saveAddressOnSharedPreference(_addressModel?.result?.items?[index].address?.road ?? "");
+                        context.read<PageController>().animateToPage(2, duration: Duration(microseconds: 700), curve: Curves.easeOut);
+                      }
+                    );
+                  }),
+            ),
+            if (_addressPointModelList.isNotEmpty) Expanded(
+              child: ListView.builder(
+                  itemCount: _addressPointModelList.length,
+                  itemBuilder: (context, index) {
+                    if (_addressPointModelList[index].result == null || _addressPointModelList[index].result!.isEmpty) {
+                      return Container();
+                    }
+
+                    return ListTile(
+                      title: Text(_addressPointModelList[index].result?[0].text ?? ""),
+                      subtitle: Text(_addressPointModelList[index].result?[0].zipcode ?? ""),
+                      onTap: () {
+                        _saveAddressOnSharedPreference(_addressPointModelList[index].result?[0].text ?? "");
+                        context.read<PageController>().animateToPage(2, duration: Duration(microseconds: 700), curve: Curves.easeOut);
+                      }
+                    );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/start/auth_page.dart
+++ lib/screens/start/auth_page.dart
@@ -87,105 +87,110 @@ class _AuthPageState extends State<AuthPage> {
                   style: Theme.of(context).textTheme.headline5,
                 ),
                 elevation: Theme.of(context).appBarTheme.elevation),
-            body: Padding(
-              padding: EdgeInsets.all(common_bg_padding),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      ExtendedImage.asset(
-                        'assets/images/security.png',
-                        width: size.width * 0.25,
-                        height: size.width * 0.25,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Expanded(
-                        child: Text(
-                          '무마켓은 전화번호로 가입합니다. 여러분의 개인정보는 안전히 보관되며 외부에 노출되지 않습니다.',
-                          style: Theme.of(context).textTheme.subtitle1,
+            body: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: Padding(
+                padding: EdgeInsets.all(common_bg_padding),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        ExtendedImage.asset(
+                          'assets/images/security.png',
+                          width: size.width * 0.25,
+                          height: size.width * 0.25,
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: common_sm_padding,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.phone,
-                    controller: _textEditingController,
-                    inputFormatters: [MaskedInputFormatter("000-0000-0000")],
-                    decoration: InputDecoration(
-                      border: inputBorder,
-                      focusedBorder: inputBorder,
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Expanded(
+                          child: Text(
+                            '무마켓은 전화번호로 가입합니다. 여러분의 개인정보는 안전히 보관되며 외부에 노출되지 않습니다.',
+                            style: Theme.of(context).textTheme.subtitle1,
+                          ),
+                        )
+                      ],
                     ),
-                    validator: (phoneNumber) {
-                      if (phoneNumber != null && phoneNumber.length == 13) {
-                        return null;
-                      } else {
-                        return '올바른 전화번호를 입력하세요.';
-                      }
-                    },
-                  ),
-                  SizedBox(
-                    height: common_sm_padding,
-                  ),
-                  Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-                    TextButton(
-                      onPressed: () {
-                        _getAddress();
-                        if (_formKey.currentState != null) {
-                          bool passed = _formKey.currentState!.validate();
-
-                          if (passed) {
-                            setState(() {
-                              _verificationStatus = VerificationStatus.codeSent;
-                            });
-                          }
+                    SizedBox(
+                      height: common_sm_padding,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.phone,
+                      controller: _textEditingController,
+                      inputFormatters: [MaskedInputFormatter("000-0000-0000")],
+                      decoration: InputDecoration(
+                        border: inputBorder,
+                        focusedBorder: inputBorder,
+                      ),
+                      validator: (phoneNumber) {
+                        if (phoneNumber != null && phoneNumber.length == 13) {
+                          return null;
+                        } else {
+                          return '올바른 전화번호를 입력하세요.';
                         }
                       },
-                      child: Text("인증 문자 발송"),
-                      style: Theme.of(context).textButtonTheme.style,
-                    )
-                  ]),
-                  SizedBox(
-                    height: common_sm_padding,
-                  ),
-                  AnimatedOpacity(
-                    duration: duration,
-                    opacity: _verificationStatus == VerificationStatus.none ? 0 : 1,
-                    child: AnimatedContainer(
-                      duration: duration,
-                      curve: Curves.easeInOut,
-                      height: getVerificationHeight(_verificationStatus),
-                      child: TextFormField(
-                        keyboardType: TextInputType.number,
-                        controller: _verifiNumberController,
-                        inputFormatters: [MaskedInputFormatter("000000")],
-                        decoration: InputDecoration(
-                          border: inputBorder,
-                          focusedBorder: inputBorder,
-                        ),
-                      ),
                     ),
-                  ),
-                  AnimatedContainer(
-                    duration: duration,
-                    height: getVerificationBtnHeight(_verificationStatus),
-                    child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                    SizedBox(
+                      height: common_sm_padding,
+                    ),
+                    Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
                       TextButton(
                         onPressed: () {
-                          attemptVerify(context);
+                          _getAddress();
+                          if (_formKey.currentState != null) {
+                            bool passed = _formKey.currentState!.validate();
+
+                            if (passed) {
+                              setState(() {
+                                _verificationStatus = VerificationStatus.codeSent;
+                              });
+                            }
+                          }
                         },
-                        child: (_verificationStatus == VerificationStatus.verifying)
-                            ? CircularProgressIndicator(color: Colors.white)
-                            : Text("인증 하기"),
+                        child: Text("인증 문자 발송"),
                         style: Theme.of(context).textButtonTheme.style,
                       )
                     ]),
-                  )
-                ],
+                    SizedBox(
+                      height: common_sm_padding,
+                    ),
+                    AnimatedOpacity(
+                      duration: duration,
+                      opacity: _verificationStatus == VerificationStatus.none ? 0 : 1,
+                      child: AnimatedContainer(
+                        duration: duration,
+                        curve: Curves.easeInOut,
+                        height: getVerificationHeight(_verificationStatus),
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          controller: _verifiNumberController,
+                          inputFormatters: [MaskedInputFormatter("000000")],
+                          decoration: InputDecoration(
+                            border: inputBorder,
+                            focusedBorder: inputBorder,
+                          ),
+                        ),
+                      ),
+                    ),
+                    AnimatedContainer(
+                      duration: duration,
+                      height: getVerificationBtnHeight(_verificationStatus),
+                      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                        TextButton(
+                          onPressed: () {
+                            attemptVerify(context);
+                          },
+                          child: (_verificationStatus == VerificationStatus.verifying)
+                              ? CircularProgressIndicator(color: Colors.white)
+                              : Text("인증 하기"),
+                          style: Theme.of(context).textButtonTheme.style,
+                        )
+                      ]),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
