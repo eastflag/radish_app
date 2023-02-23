0fb3a8af1699ad381219af69bdf2a6fbfd419f40
DK <eastflag@cnspartner.com>
2023-02-23 14:20:57 +0900

    27강 shimmer loading screen

--- lib/screens/home/items_page.dart
+++ lib/screens/home/items_page.dart
@@ -2,7 +2,9 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:radish_app/constants/common_size.dart';
+import 'package:radish_app/screens/start/auth_page.dart';
 import 'package:radish_app/utils/logger.dart';
+import 'package:shimmer/shimmer.dart';
 
 class ItemsPage extends StatelessWidget {
   const ItemsPage({Key? key}) : super(key: key);
@@ -14,31 +16,137 @@ class ItemsPage extends StatelessWidget {
       final imgSize = size.width / 4;
       logger.d(size.width, size.height);
 
-      return ListView.separated(
+      return FutureBuilder(
+        future: Future.delayed(Duration(seconds: 2)),
+        builder: (context, snapshot) {
+          return AnimatedSwitcher(
+            duration: Duration(milliseconds: 600),
+            child: (snapshot.connectionState != ConnectionState.done) ? _shimmerListView(imgSize) : _listView(imgSize);
+          );
+        }
+      );
+    });
+  }
+
+  ListView _listView(double imgSize) {
+    return ListView.separated(
+        separatorBuilder: (context, index) {
+          return Divider(
+            height: common_sm_padding * 3,
+            thickness: 2,
+            color: Colors.grey[200],
+            indent: common_sm_padding,
+            endIndent: common_sm_padding,
+          );
+        },
+        padding: EdgeInsets.all(common_sm_padding),
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return SizedBox(
+            height: imgSize,
+            child: Row(
+              children: [
+                SizedBox(
+                    height: imgSize,
+                    width: imgSize,
+                    child: CachedNetworkImage(
+                      imageUrl: 'https://picsum.photos/100',
+                      placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                    )),
+                SizedBox(
+                  width: common_bg_padding,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "그래픽 카드 RTX3060",
+                        style: Theme.of(context).textTheme.subtitle1,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "1시간전",
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text("15,000원"),
+                      Expanded(child: Container()),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SizedBox(
+                            height: 15,
+                            child: FittedBox(
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.chat_bubble_2,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                    "31",
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  Icon(
+                                    CupertinoIcons.heart,
+                                    color: Colors.grey,
+                                  ),
+                                  Text(
+                                    "31",
+                                    style: TextStyle(color: Colors.grey),
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Widget _shimmerListView(double imgSize) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      enabled: true,
+      child: ListView.separated(
           separatorBuilder: (context, index) {
             return Divider(
               height: common_sm_padding * 3,
-              thickness: 2,
+              thickness: 1,
               color: Colors.grey[200],
               indent: common_sm_padding,
               endIndent: common_sm_padding,
             );
           },
           padding: EdgeInsets.all(common_sm_padding),
-          itemCount: 10,
+          itemCount: 50,
           itemBuilder: (context, index) {
             return SizedBox(
               height: imgSize,
               child: Row(
                 children: [
-                  SizedBox(
-                      height: imgSize,
-                      width: imgSize,
-                      child: CachedNetworkImage(
-                        imageUrl: 'https://picsum.photos/100',
-                        placeholder: (context, url) =>
-                        const CircularProgressIndicator(),
-                      )),
+                  Container(
+                    height: imgSize,
+                    width: imgSize,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                  ),
                   SizedBox(
                     width: common_bg_padding,
                   ),
@@ -46,48 +154,50 @@ class ItemsPage extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          "그래픽 카드 RTX3060",
-                          style: Theme.of(context).textTheme.subtitle1,
+                        Container(
+                          height: 25,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5)
+                          ),
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Container(
+                          height: 18,
+                          width: 110,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5)
+                          ),
                         ),
                         SizedBox(
                           height: 10,
                         ),
-                        Text(
-                          "1시간전",
-                          style: Theme.of(context).textTheme.subtitle2,
+                        Container(
+                          height: 20,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(5)
+                          ),
                         ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Text("15,000원"),
                         Expanded(child: Container()),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.end,
                           children: [
-                            SizedBox(
-                              height: 15,
-                              child: FittedBox(
-                                child: Row(
-                                  children: [
-                                    Icon(
-                                      CupertinoIcons.chat_bubble_2,
-                                      color: Colors.grey,
-                                    ),
-                                    Text(
-                                      "31",
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    Icon(
-                                      CupertinoIcons.heart,
-                                      color: Colors.grey,
-                                    ),
-                                    Text(
-                                      "31",
-                                      style: TextStyle(color: Colors.grey),
-                                    )
-                                  ],
-                                ),
+                            Container(
+                              height: 16,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(5)
                               ),
                             )
                           ],
@@ -98,7 +208,7 @@ class ItemsPage extends StatelessWidget {
                 ],
               ),
             );
-          });
-    });
+          }),
+    );
   }
 }
