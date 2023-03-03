0b014132f29405a6efb04668b2a798a38da194e2
DK <eastflag@cnspartner.com>
2023-03-03 18:31:53 +0900

    34강 - inputScreen, 이미지, 삭제 버튼

--- lib/input/multi_image_select.dart
+++ lib/input/multi_image_select.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:radish_app/constants/common_size.dart';
 
@@ -11,6 +12,7 @@ class MultiImageSelect extends StatelessWidget {
     return LayoutBuilder(
       builder: (context, constraints) {
         Size _size = MediaQuery.of(context).size;
+        var imgSize = _size.width / 3;
 
         return SizedBox(
           width: _size.width,
@@ -21,44 +23,49 @@ class MultiImageSelect extends StatelessWidget {
               Padding(
                 padding: EdgeInsets.all(common_sm_padding),
                 child: Container(
-                  width: _size.width/3,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: Colors.grey, width: 2)
-                  ),
+                  width: _size.width / 3,
+                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(16), border: Border.all(color: Colors.grey, width: 2)),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Icon(Icons.camera_alt_rounded, color: Colors.grey),
-                      Text('0/10', style: Theme.of(context).textTheme.subtitle2,)
+                      Text(
+                        '0/10',
+                        style: Theme.of(context).textTheme.subtitle2,
+                      )
                     ],
                   ),
                 ),
               ),
-              Container(
-                width: _size.width/5,
-                decoration: BoxDecoration(
-                    color: Colors.red
-                ),
-              ),
-              Container(
-                width: _size.width/4,
-                decoration: BoxDecoration(
-                    color: Colors.blue
-                ),
-              ),
-              Container(
-                width: _size.width/6,
-                decoration: BoxDecoration(
-                    color: Colors.amber
-                ),
-              ),
-              Container(
-                width: _size.width/5,
-                decoration: BoxDecoration(
-                    color: Colors.cyan
-                ),
-              ),
+              ...List.generate(
+                  100,
+                  (index) => Stack(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: common_sm_padding, bottom: common_sm_padding, right: common_sm_padding),
+                            child: CachedNetworkImage(
+                              imageUrl: 'https://picsum.photos/100',
+                              width: imgSize,
+                              height: imgSize,
+                              imageBuilder: (context, imageProvider) => Container(
+                                decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
+                                    shape: BoxShape.rectangle,
+                                    borderRadius: BorderRadius.circular(16)),
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                              top: 0,
+                              right: 0,
+                              width: 40,
+                              height: 40,
+                              child: IconButton(onPressed: () {}, icon: Icon(Icons.remove_circle, color: Colors.red[400])))
+                        ],
+                      ))
             ],
           ),
         );
