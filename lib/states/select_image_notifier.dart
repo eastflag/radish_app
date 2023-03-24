1aa15b488ce8a0f664dd37fa8347a8e6dfcd39d3
DK <eastflag@cnspartner.com>
2023-03-24 19:19:13 +0900

    41강 select_image_notifier.dart ...

--- lib/states/select_image_notifier.dart
+++ lib/states/select_image_notifier.dart
@@ -12,6 +12,16 @@ class SelectImageNotifier extends ChangeNotifier {
       newImages.forEach((xfile) async {
         _images.add(await xfile.readAsBytes());
       });
+      notifyListeners();
     }
   }
+
+  void removeImage(int index) {
+    if (_images.length >= index) {
+      _images.removeAt(index);
+    }
+    notifyListeners();
+  }
+
+  List<Uint8List> get images => _images;
 }
